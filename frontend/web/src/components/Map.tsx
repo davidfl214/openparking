4fd84b44cc7a27e229bea8b781d043ce1a0cb5f4
davidfl214 <davidfl2000@gmail.com>
2025-07-04 10:57:44 +0200

    Fixed some issues

--- frontend/web/src/components/Map.tsx
+++ frontend/web/src/components/Map.tsx
@@ -7,7 +7,11 @@ import Swal from "sweetalert2";
 import type { ParkingData } from "../types/parking";
 import { fetchParkings } from "../utils/getParkingData";
 import { createStyledMarker, getMarkerColorClass } from "../utils/markerStyles";
-import { MarkerLocationClickHandler, SearchLocationHandler, UserLocationHandler } from "../utils/locationHandler";
+import {
+    MarkerLocationClickHandler,
+    SearchLocationHandler,
+    UserLocationHandler,
+} from "../utils/locationHandler";
 import { Stomp, type Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 import { LocationContext } from "../context/LocationContext";
@@ -16,19 +20,28 @@ const PARKINGS_MICROSERVICE_BASE_URL =
     import.meta.env.VITE_PARKINGS_MICROSERVICE_URL || "http://localhost:8080";
 
 export default function Map(): JSX.Element {
-    const { latitudeSearch, longitudeSearch, parkingData, isMobile, setParkingData } = useContext(LocationContext);
+    const {
+        latitudeSearch,
+        longitudeSearch,
+        parkingData,
+        isMobile,
+        setParkingData,
+    } = useContext(LocationContext);
     const { position: userLocation, error: locationError } = useGeolocation();
     const [loading, setLoading] = useState<boolean>(true);
-    const [markerLocation, setMarkerLocation] = useState<LatLngTuple | null>(null);
+    const [markerLocation, setMarkerLocation] = useState<LatLngTuple | null>(
+        null
+    );
     const stompClientRef = useRef<Client | null>(null);
-
 
     useEffect(() => {
         let stompClient: Client | null = null;
 
         const loadInitialParkings = async (): Promise<void> => {
             try {
-                const data = await fetchParkings(PARKINGS_MICROSERVICE_BASE_URL);
+                const data = await fetchParkings(
+                    PARKINGS_MICROSERVICE_BASE_URL
+                );
                 setParkingData(data);
             } catch (error) {
                 Swal.fire({
@@ -53,7 +66,7 @@ export default function Map(): JSX.Element {
 
             stompClientRef.current = stompClient;
 
-            stompClient.debug = () => {}
+            stompClient.debug = () => {};
 
             stompClient.onConnect = () => {
                 stompClient?.subscribe("/topic/parkingStatus", (message) => {
@@ -129,11 +142,14 @@ export default function Map(): JSX.Element {
             center={defaultCenter}
             zoom={14}
             scrollWheelZoom={true}
-            className="h-[85vh] w-[100vw]"
+            className="min-h-[82vh] tablet:min-h-[85vh] w-[100vw]"
         >
             <UserLocationHandler userLocation={userLocation} />
             <MarkerLocationClickHandler markerLocation={markerLocation} />
-            <SearchLocationHandler latitudeSearch={latitudeSearch} longitudeSearch={longitudeSearch} />
+            <SearchLocationHandler
+                latitudeSearch={latitudeSearch}
+                longitudeSearch={longitudeSearch}
+            />
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
