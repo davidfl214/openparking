e18d4dc0c3721f60f4e35778fa0e3741d009da7d
davidfl214 <davidfl2000@gmail.com>
2025-07-15 18:06:13 +0200

    Updated CORS config

--- microservices/parking-microservice/src/main/java/es/unir/parkingmicroservice/security/WebConfig.java
+++ microservices/parking-microservice/src/main/java/es/unir/parkingmicroservice/security/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins(allowedOrigins)
+                registry.addMapping("/**").allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
             }
 
