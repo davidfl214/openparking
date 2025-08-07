4c5c6f8e53a821b0474e8505fb419fe2aa7b75c5
davidfl214 <davidfl2000@gmail.com>
2025-08-07 18:17:11 +0200

    Added logs

--- microservices/auth-microservice/src/main/java/es/unir/authmicroservice/security/JwtAuthFilter.java
+++ microservices/auth-microservice/src/main/java/es/unir/authmicroservice/security/JwtAuthFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import es.unir.authmicroservice.service.JwtService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,7 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -33,20 +34,27 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String userEmail;
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.debug("Authorization header is missing or does not start with Bearer");
             filterChain.doFilter(request, response);
             return;
         }
 
         jwt = authHeader.substring(7);
+        log.debug("Extracted JWT: {}", jwt);
+
         userEmail = jwtService.extractUsername(jwt);
+        log.debug("Extracted user email from JWT: {}", userEmail);
 
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
             if (jwtService.isTokenValid(jwt, (User) userDetails)) {
+                log.debug("JWT is valid for user: {}", userEmail);
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                log.debug("JWT is invalid or expired for user: {}", userEmail);
             }
         }
         filterChain.doFilter(request, response);
